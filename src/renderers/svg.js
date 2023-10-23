2cb4caa1c2305c07c3b771238adeea3301a74d4b
Arnaud Juracek <arnaud.juracek@gmail.com>
2023-10-23 17:41:49 +0200

    Improve handling of <image> inside SVG

--- src/renderers/svg.js
+++ src/renderers/svg.js
@@ -1,13 +1,44 @@
-/* global XMLSerializer, btoa */
+/* global FileReader, XMLSerializer, btoa, XMLHttpRequest */
 import $ from '../utils/dom-render-svg'
 
 export default ({
   debug,
-  fonts
-}) => async (element, { x, y, width, height, style }) => $('image', {
-  x,
-  y,
-  width,
-  height,
-  href: 'data:image/svg+xml;base64,' + btoa(new XMLSerializer().serializeToString(element))
-})
+  fonts,
+  cache
+}) => async (element, { x, y, width, height, style }) => {
+  // Convert all image to dataURL to maximizime compatibility
+  for (const image of element.querySelectorAll('image[href]')) {
+    const src = image.getAttribute('href')
+
+    if (!cache.has(src)) {
+      // Fetch blob from image src
+      const blob = await new Promise(resolve => {
+        const request = new XMLHttpRequest()
+        request.open('GET', src, true)
+        request.responseType = 'blob'
+        request.onload = () => resolve(request.response)
+        request.send()
+      })
+
+      // Convert blob to dataURL using the FileReader API
+      const dataURL = await new Promise(resolve => {
+        const reader = new FileReader()
+        reader.onload = e => resolve(e.target.result)
+        reader.readAsDataURL(blob)
+      })
+
+      // Cache dataURL
+      cache.set(src, dataURL)
+    }
+
+    image.setAttribute('href', cache.get(src))
+  }
+
+  return $('image', {
+    x,
+    y,
+    width,
+    height,
+    href: 'data:image/svg+xml;base64,' + btoa(new XMLSerializer().serializeToString(element))
+  })
+}
